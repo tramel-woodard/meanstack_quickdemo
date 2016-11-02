e0b5595dff71e9232023f338fd538291e738c0d9
tramel-woodard <tramel@mindsect.com>
2016-11-02 12:44:47 -0400

    Added 'signup' routes to route.js file so user can be processed by mongodb database.

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var passport = require("passport");
 
 var User = require("./models/user");
 
@@ -21,5 +22,34 @@ router.get("/", function(req, res, next) {
             res.render("index", { users: users });
         });
 });
+
+router.get("/signup", function(req, res) {
+    res.render("signup");
+});
+
+router.post("/signup", function(req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    User.findOne({ username: username }, function(err, user) {
+        if (err) {
+            return next(err);
+        }
+        if (user) {
+            req.flash("error", "User already exists");
+            return res.redirect("/signup");
+        }
+
+        var newUser = new User({
+            username: username,
+            password: password
+        });
+        newUser.save(next);
+    });
+}, passport.authenticate("login", {
+    successRedirect: "/",
+    failureRedirect: "/signup",
+    failureFlash: true
+}));
 
 module.exports = router;
