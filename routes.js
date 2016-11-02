c6d88aab815c185380786ea5ebe0b092973c2a86
tramel-woodard <tramel@mindsect.com>
2016-11-02 13:19:05 -0400

    Updated routes.js. Added 'profile route so users can access and edit their personal information.

--- routes.js
+++ routes.js
@@ -52,4 +52,17 @@ router.post("/signup", function(req, res, next) {
     failureFlash: true
 }));
 
+router.get("/users/:username", function(req, res, next) {
+    User.findOne({ username: req.params.username },
+        function(err, user) {
+            if (err) {
+                return next(err);
+            }
+            if (!user) {
+                return next(404);
+            }
+            res.render("profile", { user: user });
+        });
+});
+
 module.exports = router;
