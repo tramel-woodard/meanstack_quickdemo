b7cade9fe7477046951e292befbc8bd7115d05f2
tramel-woodard <tramel@mindsect.com>
2016-11-02 14:16:20 -0400

    Added 'login' route to routes.js file.

--- routes.js
+++ routes.js
@@ -52,6 +52,10 @@ router.post("/signup", function(req, res, next) {
     failureFlash: true
 }));
 
+router.get("/login", function(req, res) {
+    res.render("login");
+});
+
 router.get("/users/:username", function(req, res, next) {
     User.findOne({ username: req.params.username },
         function(err, user) {
